252e92515ebf2d62601b52bd0a666e343cbe2bb9
Tony Keel <farmtireguy@gmail.com>
2020-05-04 11:05:47 -0500

    Added domain and webUI projects
    added domain (.dll) with
    added trip class to the domain
    add webui project

---
+++ Tracker2.0.Domain/Entities/Trip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracker2._0.Domain
+{
+    public class Trip
+
+    {
+        public float MileageDriven { get; set; }
+        public float GallonsUsed { get; set; }
+
+        public float CalculateMilesPerGallon()
+        {
+            return (MileageDriven / GallonsUsed);
+        }
+    }
+}
